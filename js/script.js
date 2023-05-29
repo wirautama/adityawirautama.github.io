d6c7c579c05a17e9c4dae7a7468a97c4b67de96c
Aditya Wira <wiraaditya0@gmail.com>
2023-05-30 06:48:30 +0700

    js features

--- js/script.js
+++ js/script.js
@@ -1,10 +1,28 @@
-const nav = document.getElementById('nav');
+const nav = document.getElementById('nav')
 window.addEventListener('scroll', function(){
- scrollposition = window.scrollY;
+ scrollposition = window.scrollY
  
  if (scrollposition >= 60) {
-    nav.classList.add('nav-dark');
+    nav.classList.add('nav-dark')
  } else if (scrollposition <= 60) {
-    nav.classList.remove('nav-dark');
+    nav.classList.remove('nav-dark')
  }
 })
+
+const scrollToTop = document.getElementById('scrollToTop');
+window.addEventListener('scroll', function(){
+   scrollposition = this.window.scrollY
+
+   if(scrollposition <= 1200) {
+      scrollToTop.classList.add('d-none')
+   } else if (scrollposition >= 1200) {
+      scrollToTop.classList.remove('d-none')
+   }
+})
+
+function scrollKeatas() {
+   window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+   })
+}
